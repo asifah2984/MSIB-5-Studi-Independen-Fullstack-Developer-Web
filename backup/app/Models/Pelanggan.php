71e19a1fe7d8598c3beba6b38e0302e119d3aaf4
cmd <echo>
2024-01-29 20:21:49 +0700

    Tugas Pertemuan 11 Framework Laravel

---
+++ backup/app/Models/Pelanggan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pelanggan extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'pelanggan';
+
+    protected $fillable = ['kode', 'nama', 'jk', 'tmp_lahir', 'tgl_lahir', 'email', 'kartu_id'];
+
+    public $timestamps = false;
+    
+    public function kartu(){
+        // return $this->hasOne(Kartu::class);
+        return $this->belongsTo(Kartu::class);
+    }
+}
