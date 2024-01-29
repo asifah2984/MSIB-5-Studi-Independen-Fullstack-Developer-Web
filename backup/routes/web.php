550bbf9f367326c82b4daeff225931f77c1dafb6
cmd <echo>
2024-01-29 18:04:29 +0700

    Tugas Pertemuan 4 Framework Laravel

---
+++ backup/routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LihatNilaiController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PagenotController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/salam', function () {
+    return "Assalamualaikum Selamat Belajar Laravel";
+});
+
+// tambah routing dengan parameter
+Route::get('/staff/{nama}/{divisi}', function ($nama, $divisi) {
+    return "Nama Pegawai : ".$nama. "<br> Departemen : ".$divisi;
+});
+
+// routing dengan memanggil nama file dari view
+Route::get('/kondisi', function(){
+    return view('kondisi');
+});
+
+Route::get('/nilai', function(){
+    return view('coba.nilai');
+});
+
+// routing dengan view dan array data
+Route::get('/daftarnilai', function(){
+    return view('coba.daftar');
+});
+
+// routing Memamggil dari class controller
+Route::get('/datamahasiswa', [LihatNilaiController::class, 'dataMahasiswa']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::get('/notfound', [PagenotController::class, 'index']);
