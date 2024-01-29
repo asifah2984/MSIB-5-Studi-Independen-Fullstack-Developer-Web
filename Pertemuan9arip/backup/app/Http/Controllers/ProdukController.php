c680b2e2d92376be640fb0d6a72d1a8c5ee6b390
cmd <echo>
2024-01-29 20:18:03 +0700

    Tugas Pertemuan 9 Framework Laravel

---
+++ Pertemuan9arip/backup/app/Http/Controllers/ProdukController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produk;
+use App\Models\Jenis_produk;
+use Illuminate\Support\Facades\DB;
+
+class ProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // produk berelasi dengan jenis_produk
+        $produk = Produk::join('jenis_produk', 'jenis_produk_id', '=', 'jenis_produk.id')
+        ->select('produk.*', 'jenis_produk.nama as jenis')
+        ->get();
+        return view('admin.produk.index', compact('produk'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // tambah data
+        $jenis_produk = DB::table('jenis_produk')->get();
+        return view('admin.produk.create', compact('jenis_produk'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // proses upload foto
+        if(!empty($request->foto)){
+            $fileName = 'foto-'.uniqid() . ' . ' . $request->foto->extension();     // cara 1
+            // $fileName = $request->id . ' . ' . $request->foto->extension();      // cara 2
+            $request->foto->move(public_path('admin/img'), $fileName);
+        } else{
+            $fileName = '';
+        }
+
+        // tambah data menggunakan query buider
+        DB::table('produk')->insert([
+            'kode'=>$request->kode,
+            'nama'=>$request->nama,
+            'harga_beli'=>$request->harga_beli,
+            'harga_jual'=>$request->harga_jual,
+            'stok'=>$request->stok,
+            'min_stok'=>$request->min_stok,
+            'foto'=>$fileName,
+            'deskripsi'=>$request->deskripsi,
+            'jenis_produk_id'=>$request->jenis_produk_id,
+        ]);
+        return redirect('admin/produk');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+        $produk = Produk::join('jenis_produk', 'jenis_produk_id', '=', 'jenis_produk.id')
+        ->select('produk.*', 'jenis_produk.nama as jenis')
+        ->where('produk.id',$id)
+        ->get();
+        return view('admin.produk.detail', compact('produk'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+        $jenis_produk = DB::table('jenis_produk')->get();
+        $produk = DB::table('produk')->where('id',$id)->get();
+        return view('admin.produk.edit', compact('produk', 'jenis_produk'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        // update foto
+        $foto = DB::table('produk')->select('foto')->where('id', $request->id)->get();
+        foreach($foto as $f){
+            $namaFileFotoLama = $f->foto;
+        }
+        if(!empty($request->foto)){
+            // Jika ada foto lama maka hapus fotonya
+            if(!empty( $namaFileFotoLama->foto )) unlink('admin/img' . $namaFileFotoLama->foto);
+            // Proses ganti foto
+            // $fileName = 'foto-' . uniqid() . ' . ' . $request->foto->extension();    // cara 1
+            $fileName = 'foto-' . $request->id . ' . ' . $request->foto->extension();   // cara 2
+            $request->foto->move(public_path('admin/img'), $fileName);
+        }   else{
+            $fileName = '';
+        }
+
+        // 
+        DB::table('produk')->where('id', $request->id)->update([
+            'kode'=>$request->kode,
+            'nama'=>$request->nama,
+            'harga_beli'=>$request->harga_beli,
+            'harga_jual'=>$request->harga_jual,
+            'stok'=>$request->stok,
+            'min_stok'=>$request->min_stok,
+            'foto'=>$fileName,
+            'deskripsi'=>$request->deskripsi,
+            'jenis_produk_id'=>$request->jenis_produk_id,
+        ]);
+        return redirect('admin/produk');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // 
+        DB::table('produk')->where('id', $id)->delete();
+        return redirect('admin/produk');
+    }
+}
